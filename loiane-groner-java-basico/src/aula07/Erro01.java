713e9dd6cc1b4174e7f91ef6178e2fa7cb27c5ae
anabalves <ana.alves41@fatec.sp.gov.br>
2021-12-12 14:02:43 -0300

    Aula 07 erros

---
+++ loiane-groner-java-basico/src/aula07/Erro01.java
@@ -0,0 +1,13 @@
+package aula07;
+
+public class Erro01 {
+
+	public static void main(String[] args) {
+
+//		System.out.print("Hello World!)    // Erro01.java:7: error: unclosed string literal
+//		System.out.print("Hello World!")  // Erro01.java:8: error: ';' expected
+		System.out.print("Hello World!");
+		
+	}
+
+}
---
+++ loiane-groner-java-basico/src/aula07/Erro02.java
@@ -0,0 +1,16 @@
+package aula07;
+
+public class Erro02 {
+
+	public static void main(String[] args) {
+		
+//		var int cont; // Erro02.java:7: error: not a statement
+//		int cont;
+		int cont = 0;
+		
+//		System.out.print("Você digitou: " + cont); // Erro02.java:10: error: variable cont might not have been initialized
+		System.out.print("Você digitou: " + cont);
+		
+	}
+
+}
