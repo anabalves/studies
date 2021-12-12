51ffa4c00482801e47a4b775759e0407601ceeff
anabalves <ana.alves41@fatec.sp.gov.br>
2021-12-12 16:56:26 -0300

    Aula 10 Variaveis

---
+++ loiane-groner-java-basico/src/aula10/Variaveis.java
@@ -0,0 +1,35 @@
+package aula10;
+
+public class Variaveis {
+  
+	public static void main (String[] args) {
+		
+		// convenção java
+		int idade = 20;
+		String nome = "Loiane";
+		String nomeDoMeuCachorro = "totó";
+		String ano2014 = "2014";	
+		
+		// aceito, mas não utilizado
+		int _idade;
+		int $idade;
+
+		// não é convenção Java		
+		String nome_do_meu_cachorro;
+		String NomeDoMeuCachorro;
+		String nomeDoMeucachorro;
+		
+		idade = 25;
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		System.out.println("Nome do meu cachorro = " + nomeDoMeuCachorro);
+		System.out.println("Ano = " + ano2014);
+		
+		// má prática
+		int a = 10;
+		String b = "Loiane";		
+		
+	}
+	
+}
