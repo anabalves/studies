ddeff6b2046fe4035a48466d793ccf86897e2cf8
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-02 09:50:57 -0300

    Add Exemplo3.java

---
+++ geek-university-programacao-e-informatica/java/src/Exemplo3.java
@@ -0,0 +1,13 @@
+
+public class Exemplo3 {
+	
+	public static void main(String[] args) {
+		String nome = "Pedro da Silva";
+		System.out.println("Geek University");
+		System.out.println(nome);
+		nome = "Maria da Silva";
+		System.out.println(nome);
+		
+	}
+
+}
