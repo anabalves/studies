091122d3fd01f5fa74abbb19626736c801dfdc3f
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-02 09:53:29 -0300

    Add Exemplo4.java

---
+++ geek-university-programacao-e-informatica/java/src/Exemplo4.java
@@ -0,0 +1,14 @@
+
+public class Exemplo4 {
+	
+	public static void main(String[] args) {
+		int numero = 42;
+		
+		System.out.println("O número é " + numero);
+		
+		numero += 58;
+		
+		System.out.println("O número é " + numero);
+	}
+
+}
