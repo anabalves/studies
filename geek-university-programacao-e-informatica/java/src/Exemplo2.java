2b6dc9651347ae4e8a4ea1577130ccef64ec75ae
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-02 09:49:39 -0300

    Add Exemplo2.java

---
+++ geek-university-programacao-e-informatica/java/src/Exemplo2.java
@@ -0,0 +1,11 @@
+
+public class Exemplo2 {
+	
+	public static void main(String[] args) {
+		String nome = "Pedro da Silva";
+		System.out.println("Geek University");
+		System.out.println(nome);
+		
+	}
+
+}
