c6255b5019f67f4384d4ea000cf5dff160574306
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-03 17:43:15 -0300

    Solucao do problema soma_vetores em C

---
+++ dev-superior-algoritmo-e-logica-de-programacao/c/exercicios/soma_vetores.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    
+    printf("Quantos valores vai ter cada vetor? ");
+    scanf("%d", &N);
+
+    int A[N], B[N], C[N];
+
+    printf("Digite os valores do vetor A:\n");
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &A[i]);
+    }
+
+    printf("Digite os valores do vetor B:\n");
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &B[i]);
+    }
+
+    for (int i = 0; i < N; i++) {
+        C[i] = A[i] + B[i];
+    }
+
+    printf("VETOR RESULTANTE:\n");
+    for (int i = 0; i < N; i++) {
+        printf("%d\n", C[i]);
+    }
+
+    return 0;
+}
