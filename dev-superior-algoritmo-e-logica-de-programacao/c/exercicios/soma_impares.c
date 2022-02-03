e7ed5390271e66bbd8c37879c352868f11d3cdfc
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-03 15:13:32 -0300

    Solucao do problema soma_impares em C

---
+++ dev-superior-algoritmo-e-logica-de-programacao/c/exercicios/soma_impares.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y, troca, soma;
+
+    printf("Digite dois numeros:\n");
+    scanf("%d\n%d", &X, &Y);
+
+    if (X > Y) {
+        troca = X;
+        X = Y;
+        Y = troca;
+    }
+
+    soma = 0;
+    for (int i = X+1; i <= Y-1; i++) {
+
+        if (i % 2 != 0) {
+            soma = soma + i;
+        }
+
+    }
+
+    printf("SOMA DOS IMPARES = %d\n", soma);
+
+    return 0;
+}
