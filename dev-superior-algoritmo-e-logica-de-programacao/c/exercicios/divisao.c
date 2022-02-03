37c68b8112b6aa8c2d15f5d2b06ece7d046a111f
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-03 16:53:50 -0300

    Solucao do problema divisao em C

---
+++ dev-superior-algoritmo-e-logica-de-programacao/c/exercicios/divisao.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int N, numerador, denominador;
+    double divisao;
+
+    printf("Quantos casos voce vai digitar? ");
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        printf("Entre com o numerador: ");
+        scanf("%d", &numerador);
+
+        printf("Entre com o denominador: ");
+        scanf("%d", &denominador);
+
+        if (denominador == 0) {
+            printf("DIVISAO IMPOSSIVEL\n");
+        } else {
+            divisao = (double) numerador / denominador;
+            printf("DIVISAO = %.2lf\n", divisao);
+        }
+    }
+
+    return 0;
+}
