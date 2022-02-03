8d52e25c0ab09b97a3463afaa90c4c20317eee17
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-03 17:00:37 -0300

    Solucao do problema fatorias em C

---
+++ dev-superior-algoritmo-e-logica-de-programacao/c/exercicios/fatorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int N, fatorial;
+
+    printf("Digite o valor de N: ");
+    scanf("%d", &N);
+
+    fatorial = 1;
+    for (int i = N; i > 0; i--) {
+        fatorial = fatorial * i;
+    }
+
+    printf("FATORIAL = %d\n", fatorial);
+
+    return 0;
+}
