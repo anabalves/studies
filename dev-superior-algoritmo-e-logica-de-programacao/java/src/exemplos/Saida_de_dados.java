7624ea6af58b6c4e008a97e66bf05edce65341ee
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-04 21:27:14 -0300

    Saida de dados em Java

---
+++ dev-superior-algoritmo-e-logica-de-programacao/java/src/exemplos/Saida_de_dados.java
@@ -0,0 +1,38 @@
+package exemplos;
+
+public class Saida_de_dados {
+
+    public static void main(String[] args) {
+
+        System.out.print("Bom dia");
+        System.out.print("Boa noite");
+
+        System.out.println("Bom dia");
+        System.out.println("Boa noite");
+
+        int a, b;
+        a = 10;
+        b = 20;
+        System.out.println(a);
+        System.out.println(b);
+
+        double c;
+        c = 2.3456;
+        System.out.println(String.format("%.2f", c));
+
+        int idade;
+        double salario;
+        String nome;
+        char sexo;
+
+        idade = 32;
+        salario = 4560.9;
+        nome = "Maria Silva";
+        sexo = 'F';
+
+        System.out.println("A funcionaria " + nome + ", sexo " + sexo +
+                ", ganha " + String.format("%.2f", salario) + " e tem "
+                + idade + " anos");
+
+    }
+}
