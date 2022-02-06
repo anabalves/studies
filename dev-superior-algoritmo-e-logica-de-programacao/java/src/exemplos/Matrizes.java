f13a870d717f624697b777eb3099354f2dbdd494
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-05 21:44:44 -0300

    Matrizes em Java

---
+++ dev-superior-algoritmo-e-logica-de-programacao/java/src/exemplos/Matrizes.java
@@ -0,0 +1,40 @@
+package exemplos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Matrizes {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int M, N, i, j;
+
+        System.out.print("Quantas linhas vai ter a matriz? ");
+        M = sc.nextInt();
+        System.out.print("Quantas colunas vai ter a matriz? ");
+        N = sc.nextInt();
+
+        int[][] mat = new int[M][N];
+
+        for (i = 0; i < M; i++) {
+            for (j = 0; j < N; j++) {
+                System.out.print("Elemento [" + i + "," + j + "]: ");
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("\nMATRIZ DIGITADA:");
+        for (i = 0; i < M; i++) {
+            for (j = 0; j < N; j++) {
+                System.out.println(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+
+}
