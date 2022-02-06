3581edc6185eeb496e9e2352f722a5e50d43c001
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-05 21:41:31 -0300

    Estrutura para em Java

---
+++ dev-superior-algoritmo-e-logica-de-programacao/java/src/exemplos/EstruturaPara.java
@@ -0,0 +1,30 @@
+package exemplos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EstruturaPara {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N, i, x, soma;
+
+        System.out.print("Quantos numeros serao digitados? ");
+        N = sc.nextInt();
+
+        soma = 0;
+        for (i = 1; i <= N; i++) {
+            System.out.print("Digite um numero: ");
+            x = sc.nextInt();
+            soma = soma + x;
+        }
+
+        System.out.println("SOMA = " + soma);
+
+        sc.close();
+    }
+
+}
