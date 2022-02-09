9a11490f4b60a084037a8641590f6398a956c4fc
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-08 21:24:42 -0300

    Saida de dados em csharp

---
+++ dev-superior-algoritmo-e-logica-de-programacao/csharp/exemplos/saida_de_dados/saida_de_dados/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace saida_de_dados {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Bom dia");
+            Console.Write("Boa noite");
+
+            Console.WriteLine("Bom dia");
+            Console.WriteLine("Boa noite");
+
+            int a, b;
+            a = 10;
+            b = 20;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double x;
+            x = 2.3456;
+            Console.Write(x.ToString("F2", CI));
+
+            int idade;
+            double salario;
+            string nome;
+            char sexo;
+
+            idade = 32;
+            salario = 4560.9;
+            nome = "Maria Silva";
+            sexo = 'F';
+
+            Console.WriteLine("A funcionaria " + nome + ", sexo " 
+                + sexo + ", ganha " + salario.ToString("F2", CI) +
+                " e tem " + idade + " anos");
+
+        }
+
+    }
+
+}
