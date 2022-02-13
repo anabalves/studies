5f4d52879e39c84b3e1c7a41eb768751492e4ec2
anabalves <ana.alves41@fatec.sp.gov.br>
2022-02-13 13:04:47 -0300

    Processamento de dados em Python

---
+++ nelio-alves-algoritmo-e-logica-de-programacao/python/exemplos/processamento_de_dados.py
@@ -0,0 +1,31 @@
+a:int; b:int
+a = 5
+b = 2 * a
+print(a)
+print(b)
+
+c: int
+d: float
+c = 5
+d = 2 * c
+print(c)
+print(f"{d:.1f}")
+
+b1: float; b2: float; h: float; area: float
+b1 = 6.0
+b2 = 8.0
+h = 5.0
+area = (b1 + b2) / 2.0 * h
+print(area)
+
+e: int; f: int; resultado: int
+e = 5
+f = 2
+resultado = e // f
+print(resultado)
+
+i: float
+j: int
+i = 5.0
+j = int(i)
+print(j)
