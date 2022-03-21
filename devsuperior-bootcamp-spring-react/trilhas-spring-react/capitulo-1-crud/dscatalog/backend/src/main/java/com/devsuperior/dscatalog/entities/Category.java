02524e2056ac5f00cfabd76d00f47ae24da8d2d4
anabalves <ana.alves41@fatec.sp.gov.br>
2022-03-21 08:35:08 -0300

    H2 database

--- devsuperior-bootcamp-spring-react/trilhas-spring-react/capitulo-1-crud/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ devsuperior-bootcamp-spring-react/trilhas-spring-react/capitulo-1-crud/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,25 +1,27 @@
 package com.devsuperior.dscatalog.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
-    public Category() {
-    }
+    public Category() { }
 
     public Category(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
 
     public void setId(Long id) {
         this.id = id;
