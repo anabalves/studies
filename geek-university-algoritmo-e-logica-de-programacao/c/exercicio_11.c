3660ed2e60ae9efd200bb0359af35c571adab204
anabalves <ana.alves41@fatec.sp.gov.br>
2022-07-25 08:42:13 -0300

    Exercicio 11

---
+++ geek-university-algoritmo-e-logica-de-programacao/c/exercicio_11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	float altura, peso_ideal;
+
+	printf("Qual sua altura? ");
+	scanf("%f", &altura);
+
+	peso_ideal = (72.7 * altura) - 58;
+
+	printf("Seu peso ideal seria %.2f", peso_ideal);
+}
