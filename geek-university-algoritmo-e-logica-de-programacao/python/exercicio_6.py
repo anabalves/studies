5b42258d2d25e8d2114953a54177b486c92bf6a1
anabalves <ana.alves41@fatec.sp.gov.br>
2022-07-25 08:52:27 -0300

    Exercicio 6

---
+++ geek-university-algoritmo-e-logica-de-programacao/python/exercicio_6.py
@@ -0,0 +1,6 @@
+quantidade_minima = int(input("Informe a quantidade minima: "))
+quantidade_maxima = int(input("Informe a quantidade maxima: "))
+
+estoque_medio = (quantidade_minima + quantidade_maxima) / 2
+
+print("O estoque medio eh {0}".format(estoque_medio))
