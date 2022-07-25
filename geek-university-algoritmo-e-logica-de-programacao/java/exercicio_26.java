703fe43c914a980a3e159ac4cd28e2d849049a51
anabalves <ana.alves41@fatec.sp.gov.br>
2022-07-25 08:50:35 -0300

    Exercicio 26

---
+++ geek-university-algoritmo-e-logica-de-programacao/java/exercicio_26.java
@@ -0,0 +1,11 @@
+public class exercicio_26 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 10 == 0) {
+                System.out.println(i + " eh multiplo de 10");
+            }
+        }
+    }
+
+}
