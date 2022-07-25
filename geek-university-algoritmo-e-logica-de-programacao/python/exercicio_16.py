24060cf1a7727d63e35cd6d060be0d76fb916550
anabalves <ana.alves41@fatec.sp.gov.br>
2022-07-25 08:52:59 -0300

    Exercicio 16

---
+++ geek-university-algoritmo-e-logica-de-programacao/python/exercicio_16.py
@@ -0,0 +1,13 @@
+numero = int(input("Informe um numero: "))
+
+if numero == 0:
+    print("Valor neutro")
+    print(numero)
+elif numero > 0:
+    a = numero
+    print("Valor positivo")
+    print(a)
+else:
+    b = numero
+    print("Valor negativo")
+    print(b)
