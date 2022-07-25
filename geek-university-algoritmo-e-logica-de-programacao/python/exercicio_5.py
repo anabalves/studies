7c9557c42e27de34c0b0b8d746b04c92a8b57e3d
anabalves <ana.alves41@fatec.sp.gov.br>
2022-07-25 08:52:22 -0300

    Exercicio 5

---
+++ geek-university-algoritmo-e-logica-de-programacao/python/exercicio_5.py
@@ -0,0 +1,8 @@
+codigo = int(input("Informe o codigo da peca: "))
+valor = float(input("Informe o valor da peca: "))
+qtd = int(input("Informe a quantidade de pecas: "))
+
+total = qtd * valor
+
+print("Codigo da peca: {0}".format(codigo))
+print("Valor total: {0}".format(total))
