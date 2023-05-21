ae50031c93e534f15892f040a0a9dadc45f5aef1
anabalves <ana.alves41@fatec.sp.gov.br>
2023-05-20 23:19:49 -0300

    Delete category

---
+++ devsuperior/bootcamp-spring-react/README.md
@@ -0,0 +1,3 @@
+# DevSuperior - Bootcamp Spring React
+
+Nesse repositório estão as atividades desenvolvidas no Bootcamp Spring React disponivel e sob orientação da escola DevSuperior.
--- devsuperior/bootcamp-spring-react/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ devsuperior/bootcamp-spring-react/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dscatalog.entities.Category;
@@ -51,6 +54,16 @@ public class CategoryService {
             return new CategoryDTO(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id Not Found " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id Not Found " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
         }
     }
 
